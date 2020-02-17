f13a858eae4ace7f18602b26f4c9e50e439567d5
bakeiro <davixt3@gmail.com>
2020-02-17 16:31:12 +0100

    update users login

--- src/Form/UserEditForm.php
+++ src/Form/UserEditForm.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -17,11 +18,16 @@ class UserEditForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add("id", HiddenType::class, ['mapped' => false]);
+        $builder->add("email", TextType::class);
+        $builder->add("password", PasswordType::class);
+        $builder->add("roles", ChoiceType::class, [
+        "choices" => ['ROLE_ADMINMASTER','ROLE_USER'],
+        'multiple' => true,
+        'expanded' => true
+        ]);
         $builder->add("name", TextType::class);
         $builder->add("last_name", TextType::class);
-        $builder->add("email", TextType::class);
         $builder->add("age", NumberType::class);
-        $builder->add("password", PasswordType::class);
         $builder->add("save", SubmitType::class, ["label" => "Update user"]);
     }
 
