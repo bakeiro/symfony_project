18e645d430384e96118f811e1ee89a9de0abef37
bakeiro <davixt3@gmail.com>
2020-02-18 16:01:47 +0100

    improved login and security

--- src/Controller/UserLoginController.php
+++ src/Controller/UserLoginController.php
@@ -9,9 +9,6 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserLoginController extends AbstractController
 {
-    /**
-     * @Route("/login", name="app_login")
-     */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
@@ -26,9 +23,6 @@ class UserLoginController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    /**
-     * @Route("/logout", name="app_logout")
-     */
     public function logout()
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
