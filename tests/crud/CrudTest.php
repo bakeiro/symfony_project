fc09f367b6aff68f2b71dfe0d76fbd8c80bfb8f0
davidbaqueiro <davidbaqueiro@outlook.com>
2020-03-24 12:56:23 +0100

    bwoa

--- tests/crud/CrudTest.php
+++ tests/crud/CrudTest.php
@@ -5,8 +5,9 @@ namespace App\Tests\Crud;
 use Monolog\Test\TestCase;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class CrudTest extends TestCase
+class CrudTest extends WebTestCase
 {
     private $objectManager;
 
@@ -15,20 +16,49 @@ class CrudTest extends TestCase
         $this->objectManager = $objectManager;
     }
 
+    public function logIn()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/login');
+
+        $login_form = $client->selectButton('.ui.button.primary')->form();
+
+        // TODO: What about don't hardcode this values?
+        $form["email"] = "adminmaster@email.com";
+        $form["password"] = "thisIsAPassword";
+
+        $client = $form->submit($form); // Logged?
+    }
+
     public function testGetUsers()
     {
+        $this->logIn();
+
+        $client = static::createClient();
+        $client->request('GET', '/crud_test');
+
+        
+
+        /*
         $user_repository = $this->objectManager->getRepository(User::class);
         $all_users = $user_repository->findAll();
 
         $count_all_users = count($all_users);
 
-        // Get the welcome page and count how many divs??
-        
+        $client = static::createClient();
 
+        // Log in
+
+        // See how many divs
+        
+        // button.ui.default.button.mini.edit_button
+
+        */
     }
 
     public function testAddUser()
     {
+        $user_to_insert = new User();
 
     }
 
